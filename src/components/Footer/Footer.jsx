04e42e120fd67557b6203fd915384609e21bf407
kitros <kitcotros2@gmail.com>
2025-11-03 12:53:47 -0600

    Add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import "./Footer.css";
 
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <div className="footer">
+        <div className="footer__left">Developed by Kit Cotros</div>
+        <div className="footer__right">2025</div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
